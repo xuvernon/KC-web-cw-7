87c7396711c087d8511d0495d63625da9726b3b0
sarah almutari <sara0moha78@gmail.com>
2022-08-18 23:03:36 +0300

    .

--- script.js
+++ script.js
@@ -9,17 +9,22 @@ console.log(BMI(70,1.70))
 
 
 
-
+let div = document.getElementById(`result`)
 
 
 
 function Status(BMI){
+   
+
 if(BMI < 18.5) {
+    div.style.color = "orange";
  return `لديك نقص في الوزن`
  
 } else if(BMI >=18.5 && BMI < 25){
+    div.style.color = "green"
     return ` وزنك صحي`
 } else if(BMI >= 25 ){
+    div.style.color = "red"
     return "لديك زياده في الوزن"
 }
 }
@@ -39,7 +44,6 @@ let height = document.getElementById(`height`).value
 let bmi = BMI(weight,height)
 let desc = Status(BMI(weight,height))
 
-let div = document.getElementById(`result`)
 div.innerText = bmi + " == " + desc
 }
 
